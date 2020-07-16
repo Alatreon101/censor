46e3213508772c03acb3899d4746754b7092ca0a
FASfalcon <alexanderfedasov@gmail.com>
2020-07-16 17:04:11 +0300

    C update2.0 - изменен функционал

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,8 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         String input = sc.nextLine();
-        final String[] split = input.split(" ");
-
-        for (int i = 0; i < split.length; i++) {
-
-            if ("бяка".equals(split[i].toLowerCase())) {
-                split[i] = "вырезано цензурой";
-            }
-        }
-
-        System.out.println(String.join(" ", split));
+        input = input.toLowerCase();
+        System.out.println(input.replaceAll("\\b(бяка)\\b", " - вырезано цензурой!"))
     }
 }
